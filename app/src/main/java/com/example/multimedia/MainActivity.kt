2999c842875c14913ef4c81f9a0504069758c716
arndri <hardwarebuild041100@gmail.com>
2024-06-09 20:51:04 +0700

    Initial commit

---
+++ app/src/main/java/com/example/multimedia/MainActivity.kt
@@ -0,0 +1,111 @@
+package com.example.multimedia;
+
+import android.Manifest
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.AspectRatio
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import com.example.multimedia.databinding.ActivityMainBinding
+import org.tensorflow.lite.task.vision.classifier.Classifications
+import java.text.NumberFormat
+import java.util.concurrent.Executors
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var imageClassify: ImageClassify
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            val message = if (isGranted) "Ijin kamera diberikan" else "Ijin kamera ditolak"
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+
+        startCamera()
+
+    }
+
+    private fun startCamera() {
+
+        imageClassify =
+            ImageClassify(
+                context = this,
+                imageClassifierListener = object : ImageClassify.ClassifierListener {
+                    override fun onError(error: String) {
+                        runOnUiThread {
+                            Toast.makeText(this@MainActivity, error, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                    override fun onResults(results: List<Classifications>?, inferenceTime: Long) {
+                        runOnUiThread {
+                            results?.let { it ->
+                                if (it.isNotEmpty() && it[0].categories.isNotEmpty()) {
+                                    println(it)
+                                    val sortedCategories =
+                                        it[0].categories.sortedByDescending { it?.score }
+                                    val displayResult =
+                                        sortedCategories.joinToString("\n") {
+                                            "${it.label} " + NumberFormat.getPercentInstance().format(it.score).trim()
+                                        }
+                                    binding.tvResult.text = displayResult
+                                }
+                            }
+                        }
+                    }
+                })
+
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder()
+                .setTargetAspectRatio(AspectRatio.RATIO_4_3)
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+
+            val imageAnalyzer =
+                ImageAnalysis.Builder()
+                    .setTargetAspectRatio(AspectRatio.RATIO_4_3)
+                    .setTargetRotation(binding.viewFinder.display.rotation)
+                    .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                    .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
+                    .build()
+                    .also {
+                        it.setAnalyzer(Executors.newSingleThreadExecutor()) { image ->
+                            imageClassify.classify(image)
+                        }
+                    }
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(
+                    this,
+                    CameraSelector.DEFAULT_BACK_CAMERA,
+                    preview,
+                    imageAnalyzer
+                )
+            } catch (exc: Exception) {
+                Toast.makeText(this, "Gagal memunculkan kamera.", Toast.LENGTH_SHORT).show()
+            }
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+
+}
